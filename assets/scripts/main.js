a1f25e456f8bb7014f92ee435d9ccf841e7a2a65
Ashwin Rohit <aalagiri@ucsd.edu>
2022-11-12 00:30:18 -0800

    Complete Explore Section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -21,7 +21,6 @@ async function init() {
   let recipes;
   try {
     recipes = await getRecipes();
-    console.log(recipes);
   } catch (err) {
     console.error(err);
   }
@@ -46,15 +45,31 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+  if (!('serviceWorker' in navigator)) {
+    console.error('Service Workers are not supported in this browser');
+  }
   // B2. TODO - Listen for the 'load' event on the window object.
-  // Steps B3-B6 will be *inside* the event listener's function created in B2
-  // B3. TODO - Register './sw.js' as a service worker (The MDN article
-  //            "Using Service Workers" will help you here)
-  // B4. TODO - Once the service worker has been successfully registered, console
-  //            log that it was successful.
-  // B5. TODO - In the event that the service worker registration fails, console
-  //            log that it has failed.
-  // STEPS B6 ONWARDS WILL BE IN /sw.js
+  window.addEventListener('load', (event) => {
+    // Steps B3-B6 will be *inside* the event listener's function created in B2
+    // B3. TODO - Register './sw.js' as a service worker (The MDN article
+    //            "Using Service Workers" will help you here)
+    // B4. TODO - Once the service worker has been successfully registered, console
+    //            log that it was successful.
+    // B5. TODO - In the event that the service worker registration fails, console
+    //            log that it has failed.
+    // STEPS B6 ONWARDS WILL BE IN /sw.js
+    let registration;
+    navigator.serviceWorker
+      .register('/sw.js', { scope: '/' })
+      .then(serviceWorker => {
+        registration = serviceWorker;
+        console.log('Service Worker Registered', registration);
+      })
+      .catch(err => {
+        console.error('Cannot register service worker', err);
+      });
+    }
+  )
 }
 
 /**
@@ -111,16 +126,16 @@ async function getRecipes() {
     //            resolve() method.
     // A10. TODO - Log any errors from catch using console.error
     // A11. TODO - Pass any errors to the Promise's reject() function
-    for(let url of RECIPE_URLS) {
+    for (let url of RECIPE_URLS) {
       try {
         let json_data = await ((await fetch(url)).json());
         recipes.push(json_data);
-      } catch(error) {
+      } catch (error) {
         console.error(error);
         reject(error);
       }
     }
-    if(recipes.length != RECIPE_URLS.length) {
+    if (recipes.length != RECIPE_URLS.length) {
       const error_msg = 'Not all recipes received from servers';
       console.error(error_msg);
       reject(error_msg);
--- sw.js
+++ sw.js
@@ -9,13 +9,23 @@ self.addEventListener('install', function (event) {
     caches.open(CACHE_NAME).then(function (cache) {
       // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
       //            added to the cache when the ServiceWorker is installed
-      return cache.addAll([]);
+      const RECIPE_URLS = [
+        'https://introweb.tech/assets/json/1_50-thanksgiving-side-dishes.json',
+        'https://introweb.tech/assets/json/2_roasting-turkey-breast-with-stuffing.json',
+        'https://introweb.tech/assets/json/3_moms-cornbread-stuffing.json',
+        'https://introweb.tech/assets/json/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+        'https://introweb.tech/assets/json/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+        'https://introweb.tech/assets/json/6_one-pot-thanksgiving-dinner.json',
+      ];
+      return cache.addAll(RECIPE_URLS);
     })
   );
+  self.skipWaiting();
 });
 
 // Activates the service worker
 self.addEventListener('activate', function (event) {
+  console.log('Here');
   event.waitUntil(self.clients.claim());
 });
 
@@ -34,7 +44,13 @@ self.addEventListener('fetch', function (event) {
   /*******************************/
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
+  
+  /***
+   * The Cache is opened inside the match below.
+   ***/
+  
   // B8. TODO - If the request is in the cache, return with the cached version.
   //            Otherwise fetch the resource, add it to the cache, and return
   //            network response.
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
